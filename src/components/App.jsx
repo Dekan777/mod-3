9ed0d02678916abb18ea43727c546a1eb54da40d
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-13 18:39:13 +0100

    Select coffee size

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,48 @@
 import './App.css';
 // import { SearchBar } from './SearchBar/SearchBar';
 import { useState } from 'react';
-import { LangSwitcher } from './LangSwitcher/LangSwitcher';
+// import { LangSwitcher } from './LangSwitcher/LangSwitcher';
 
 export const App = () => {
-  const [lang, setLang] = useState('uk');
+  const [coffeeSize, setCoffeeSize] = useState('sm');
+
+  const handleSizeChange = evt => {
+    setCoffeeSize(evt.target.value);
+  };
 
   return (
     <>
-      <p>Selected language: {lang}</p>
-      <LangSwitcher value={lang} onSelect={setLang} />
+      <h1>Select coffee size</h1>
+      <label>
+        <input
+          type="radio"
+          name="coffeeSize"
+          value="sm"
+          checked={coffeeSize === 'sm'}
+          onChange={handleSizeChange}
+        />
+        Small
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="coffeeSize"
+          value="md"
+          checked={coffeeSize === 'md'}
+          onChange={handleSizeChange}
+        />
+        Meduim
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="coffeeSize"
+          value="lg"
+          checked={coffeeSize === 'lg'}
+          onChange={handleSizeChange}
+        />
+        Large
+      </label>
     </>
   );
 };
