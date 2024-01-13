635a270c8d715d508e6d9838b029b94cc4e5c55b
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-13 02:02:26 +0100

    input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-
+import { LoginForm } from './LoginForm/LoginForm';
 export const App = () => {
   return (
     <>
-      <ul className="conteinerApp"></ul>
+      <ul>
+        <LoginForm />
+      </ul>
     </>
   );
 };
