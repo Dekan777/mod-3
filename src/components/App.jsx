99c27966d49fe26365d8b7a9f15d1ab319bef22a
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-13 18:26:04 +0100

    LangSwitcher

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 // import { SearchBar } from './SearchBar/SearchBar';
+import { useState } from 'react';
 import { LangSwitcher } from './LangSwitcher/LangSwitcher';
 
 export const App = () => {
+  const [lang, setLang] = useState('uk');
+
   return (
-    <div>
-      <LangSwitcher />
-    </div>
+    <>
+      <p>Selected language: {lang}</p>
+      <LangSwitcher value={lang} onSelect={setLang} />
+    </>
   );
 };
