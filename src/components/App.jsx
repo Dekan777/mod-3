bd4322df16f32b021f438b4e335d1fde80947a06
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-22 23:21:26 +0100

    loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,56 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { ThreeDots } from 'react-loader-spinner';
+
+const ArticleList = ({ items }) => (
+  <ul>
+    {items.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
 
 export const App = () => {
-  return <div></div>;
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setLoading(true);
+        const response = await axios.get('https://hn.algolia.com/api/v1/search?query=react');
+        setArticles(response.data.hits);
+      } catch (error) {
+        // Тут будемо обробляти помилку
+      } finally {
+        // 2. Встановлюємо індикатор в false після запиту
+        setLoading(false);
+      }
+    }
+
+    fetchArticles();
+  }, []);
+
+  return (
+    <div>
+      <h1>Latest articles</h1>
+      {loading && (
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
 };
