d2d7ae042dcd8bafd2edb0e604ec5777d7c87382
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-23 22:56:32 +0100

    loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Audio } from 'react-loader-spinner';
 
 const ArticleList = ({ items }) => (
   <ul>
@@ -15,20 +16,20 @@ const ArticleList = ({ items }) => (
 
 export const App = () => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchArticles() {
       try {
+        setLoading(true);
         const response = await axios.get('https://hn.algolia.com/api/v1/search?query=react');
         console.log(response);
-
-        if (response.data) {
-          setArticles(response.data.hits);
-        } else {
-          console.error('Unexpected response format');
-        }
+        setArticles(response.data.hits);
       } catch (error) {
         console.error('Error fetching articles:', error);
+      } finally {
+        // 2. Встановлюємо індикатор в false після запиту
+        setLoading(false);
       }
     }
 
@@ -39,6 +40,17 @@ export const App = () => {
     <div>
       <h1>Latest articles</h1>
 
+      {loading && (
+        <Audio
+          height="100"
+          width="100"
+          color="#4fa94d"
+          ariaLabel="audio-loading"
+          wrapperStyle={{}}
+          wrapperClass="wrapper-class"
+          visible={true}
+        />
+      )}
       {articles.length > 0 && <ArticleList items={articles} />}
     </div>
   );
